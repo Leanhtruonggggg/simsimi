a137851f988b2d1a31af8b1a3050a8d84f880bfc
Leanhtruonggggg <88928444+Leanhtruonggggg@users.noreply.github.com>
2023-01-26 22:41:29 +0700

    Add files via upload

--- test/index.js
+++ test/index.js
@@ -2,11 +2,8 @@ const simsimi = require('../src/index.js');
 
 (async () => {
   try {
-    while (true) {
         const response = await simsimi.simtalk('hi', 'en');
         console.log(response);
-    }
-    
   }
   catch(e) {
       console.error(e);
